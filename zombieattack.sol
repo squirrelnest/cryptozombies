990d19a0bffcdccd302c76bd334df8d90f3f7772
Mikka <mikkasoft@gmail.com>
2025-01-27 13:23:15 +0800

    win/loss logic

--- zombieattack.sol
+++ zombieattack.sol
@@ -9,12 +9,18 @@ contract ZombieAttack is ZombieHelper {
   function randMod(uint _modulus) internal returns(uint) {
     randNonce++;
     return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
-
   }
 
   function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
     Zombie storage enemyZombie = zombies[_targetId];
     uint rand = randMod(100);
+    
+    if (rand <= attackVictoryProbability) {
+      myZombie.winCount++;
+      myZombie.level++;
+      enemyZombie.lossCount++;
+      feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+    } 
   }
 }
