d7a25200e1e2e6a451cbc7c1244064bf7b433391
Mikka <mikkasoft@gmail.com>
2025-01-29 17:08:43 +0800

    Update zombiehelper to use safemath

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -22,10 +22,10 @@ contract ZombieHelper is ZombieFeeding {
 
   function levelUp(uint _zombieId) external payable {
     require(msg.value == levelUpFee);
-    zombies[_zombieId].level++;
+    zombies[_zombieId].level = zombies[_zombieId].level.add(1);
   }
 
-  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) ownerOf(_zombieId) {
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) onlyOwnerOf(_zombieId) {
     zombies[_zombieId].name = _newName;
   }
 
