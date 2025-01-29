f9e87a88c11c463243eecbaa0ced414e3f15d2da
Mikka <mikkasoft@gmail.com>
2025-01-29 17:15:44 +0800

    add natspec

--- zombieownership.sol
+++ zombieownership.sol
@@ -4,6 +4,9 @@ import "./zombieattack.sol";
 import "./erc721.sol";
 import "./safemath.sol";
 
+/// @title a contract that manages transferring zombie ownership
+/// @author squirrelnest
+/// @dev compliant with OpenZeppelin's implementation of the ERC721 spec draft
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
   using SafeMath for uint256;
@@ -20,7 +23,7 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
 
   function _transfer(address _from, address _to, uint256 _tokenId) private {
     ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
-    ownerZombieCount[_from] = ownerZombieCount[_from].sub(1);
+    ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].sub(1);
     zombieToOwner[_tokenId] = _to;
     emit Transfer(_from, _to, _tokenId);
   }
